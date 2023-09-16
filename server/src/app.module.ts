54c215d4e378ca7a8b12d5c2b23b2106d02fd746
RedBandana <aurelende@duck.com>
2023-09-16 19:05:00 -0400

    Main modules added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,17 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { RequestModule } from './request/request.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_CONN_STRING),
+    UserModule, 
+    RequestModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
